bdcba86bb6f8698069e7fc4f1f7d5a30806beb52
kalen <kalen>
2018-02-02 17:10:27 +0800

    .

--- src/main/java/com/magic/weijd/util/TextMessage.java
+++ src/main/java/com/magic/weijd/util/TextMessage.java
@@ -6,6 +6,6 @@ package com.magic.weijd.util;
 public class TextMessage {
 
     /**验证码类*/
-    public static final String MSG_CODE = "【易信缘】您的验证码是{0}。如非本人操作，请忽略本短信";
+    public static final String MSG_CODE = "【欢喜直通车】您的验证码是{0}。如非本人操作，请忽略本短信";
 
 }
